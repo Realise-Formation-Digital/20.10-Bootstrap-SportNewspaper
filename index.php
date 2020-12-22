d60b5f6ff82ee0bc02416a3fece791bb7c7e9a54
Martinho <martinhowork92@gmail.com>
2020-12-22 10:36:36 +0100

    decoupage du header / footer insertion dans le index.php

--- index.php
+++ index.php
@@ -27,108 +27,9 @@
   <div class="container-scroller">
 
     <div class="main-panel">
-
-      <!-- Entête avec le logo de la page-->
-      <header id="header">
-        <div class="container">
-
-
-
-
-          <!-- navbar.html -->
-          <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="d-flex justify-content-between align-items-center navbar-top">
-              <ul class="navbar-left">
-                <li>
-                  <a target="_blank" href="https://hotelmix.fr/weather/geneva-30643%22%3E"><img
-                      src="https://w.bookcdn.com/weather/picture/13_30643_1_3_fff5d9_158_fff5d9_333333_08488D_3_fff5d9_333333_1_6.png?scode=124&domid=581&anc_id=95495"
-                      alt="booked.net" /></a>
-                </li>
-              </ul>
-
-              <!-- Logo de la page -->
-              <div>
-                <a class="navbar-brand" href="#"><img src="assets/images/logoSarss.png" alt="logo sarsport" /></a>
-                <h5 class="m-0 font-weight-600 mr-3 d-none d-lg-flex">Ton journal sportif où tu veux quand tu veux</h5>
-              </div>
-              <div class="d-flex">
-                <ul class="social-media">
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-instagram"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-youtube"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-linkedin"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-twitter"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="navbar-bottom-menu">
-              <button class="navbar-toggler" type="button" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-
-              <div class="navbar-collapse justify-content-center collapse" id="navbarSupportedContent_adrian">
-                <button class="navbar-toggler" type="button" data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <ul class="navbar-nav d-lg-flex justify-content-between align-items-center">
-                  <li>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link blanc" href="index.html">Accueil</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link blanc" href="pages/football.html">Football</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link blanc" href="pages/hockey.html">Hockey</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link blanc" href="pages/tennis.html">Tennis</a>
-                  </li>
-                  <li class="nav-item active">
-                    <a class="nav-link active blanc" href="pages/aboutus.html">Qui sommes nous ?</a>
-                  </li>
-                  <!--LOGIN-->
-                  <li class="nav-item">
-                    <a href="#contactModal" class="nav-link blanc" data-toggle="modal">Contact</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#loginModal" class="nav-link blanc" data-toggle="modal">Login</a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a href="#newPageModal" class="nav-link blanc" data-toggle="modal">Créer une page</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-
-          <!-- partial -->
-        </div>
-      </header><br>
+<?php 
+require "pages/header.php";
+?>
 
       <div class="container">
         <div class="row">
@@ -572,76 +473,10 @@
         </div>
       </div>
       <!--../------------------- ../new Page Modal HTML --------------------->
-
-      <!-- footer-->
-      <footer>
-        <div class="container">
-
-          <div class="row">
-            <!-- Logo pied de page -->
-            <div class="col-sm-4">
-              <div class="d-flex justify-content-between">
-                <img src="assets/images/logoSarss.png" class="footer-logo" alt="" />
-              </div>
-            </div>
-
-            <!-- Copyright -->
-            <div class="col-sm-4">
-              <div class="d-lg-flex justify-content-between align-items-center  mt-5 footer-bottom">
-                <p class="font-weight-medium">
-                  © 2020 @ Sarsport, Inc.All Rights Reserved.
-                </p>
-              </div>
-            </div>
-
-            <!-- Suivez-nous -->
-            <div class="col-sm-4">
-              <div class="d-flex justify-content-end footer-social">
-                <h5 class="m-0 font-weight-600 mr-3 d-none d-lg-flex">Suivez-nous:</h5>
-                <ul class="social-media">
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-instagram"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-youtube"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-linkedin"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="mdi mdi-twitter"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
-            </div>
-          </div>
-
-
-        </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-          </script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-          integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-          </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-          integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
-          </script>
-      </footer>
+<?php
+require"pages/footer.php"
+?>
+    
 
       <!-- partial -->
     </div>
@@ -656,6 +491,7 @@
     <script src="./assets/js/demo.js"></script>
     <!-- End custom js for this page-->
   </div>
+
 </body>
 
 </html>
